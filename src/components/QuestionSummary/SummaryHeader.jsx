31d16822f8978c0d6cebe3864324075d6ecfc778
SETI108 <seti108@gmail.com>
2023-11-19 11:19:40 +0100

    Add the timer to the summary

--- src/components/QuestionSummary/SummaryHeader.jsx
+++ src/components/QuestionSummary/SummaryHeader.jsx
@@ -1,22 +1,29 @@
-import { Progress } from "../Progress"
+import { Progress } from "../Progress";
+import { Timer } from "../Timer";
 import { useSelector } from "react-redux";
-
 
 export const SummaryHeader = () => {
   const answers = useSelector((state) => state.quiz.answers);
-  const totalCorrectAnswers = answers.reduce((total, { isCorrect }) => total + isCorrect, 0);
+  const totalCorrectAnswers = answers.reduce(
+    (total, { isCorrect }) => total + isCorrect,
+    0
+  );
 
-  const totalAnswers= answers.length;
-  console.log("header", totalCorrectAnswers)
+  const totalAnswers = answers.length;
+  console.log("header", totalCorrectAnswers);
   return (
     <div className="summary-header-container">
-        <Progress />
-        <div className="summary-header-content">
-          <p>
-              You got {totalCorrectAnswers}/{totalAnswers} answers correct!
-      Not quite there for being the champ.
-      </p>
+      <Progress />
+      <div className="progress-timer-div">
+        <div className="timer-div">
+          <Timer />
         </div>
+      </div>
+      <div className="summary-header-content">
+        <p>
+          You got {totalCorrectAnswers}/{totalAnswers} answers correct!
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
