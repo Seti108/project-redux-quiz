572864318ebd9d5705b1bc0ff5066c07a8fc21e0
SETI108 <seti108@gmail.com>
2023-11-19 11:21:02 +0100

    Change static text

--- src/components/QuestionSummary/SummaryMain.jsx
+++ src/components/QuestionSummary/SummaryMain.jsx
@@ -1,22 +1,22 @@
-import { SummaryAccordion } from "./SummaryAccordion"
+import { SummaryAccordion } from "./SummaryAccordion";
 import { useDispatch } from "react-redux";
 import { quiz } from "../../reducers/quiz";
 
 export const SummaryMain = () => {
-    const dispatch = useDispatch();
-    const restartQuiz = () => {
-      dispatch(quiz.actions.restart());
-    };
-    const newQuiz = () => {
-      location.reload();
-    };
+  const dispatch = useDispatch();
+  const restartQuiz = () => {
+    dispatch(quiz.actions.restart());
+  };
+  const newQuiz = () => {
+    location.reload();
+  };
   return (
     <section className="summary-main-container">
-    <SummaryAccordion />
-    <section className="button-wrapper">
+      <SummaryAccordion />
+      <section className="button-wrapper">
         <button onClick={restartQuiz}>Try again!</button>
-        <button onClick={newQuiz}>Try new quiz</button>
+        <button onClick={newQuiz}>Try random quiz</button>
       </section>
     </section>
-  )
-}
+  );
+};
